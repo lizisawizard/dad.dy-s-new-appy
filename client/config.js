1630b6f7cee67616f9785413c6daef9f997c354b
sahupr <sahupr@mail.uc.edu>
2020-11-14 15:22:12 -0500

    proxy node setup

---
+++ client/config.js
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const msalConfig = {
+  auth: {
+    clientId: '816bc2d3-3b4a-4239-9b69-9bdd414271f4',
+    redirectUri: 'http://localhost:8000/authcomplete'
+  }
+}
+
+const apiScopes = ['api://40cd0f57-fa95-4ffa-8ade-a8b301f5bb7c/.default']
