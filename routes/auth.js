1f695168859173dd62ddcbb362a543a783a0461f
jashgada <jashgada93@gmail.com>
2020-11-14 13:26:14 -0500

    set up using sdk

--- routes/auth.js
+++ routes/auth.js
@@ -1,79 +1,44 @@
-var router = require('express-promise-router')();
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
 
 /* GET auth callback. */
 router.get('/signin',
-  async function (req, res) {
-    const urlParameters = {
-      scopes: process.env.OAUTH_SCOPES.split(','),
-      redirectUri: process.env.OAUTH_REDIRECT_URI
-    };
-
-    try {
-      const authUrl = await req.app.locals
-        .msalClient.getAuthCodeUrl(urlParameters);
-      res.redirect(authUrl);
-    }
-    catch (error) {
-      console.log(`Error: ${error}`);
-      req.flash('error_msg', {
-        message: 'Error getting auth URL',
-        debug: JSON.stringify(error, Object.getOwnPropertyNames(error))
-      });
-      res.redirect('/');
-    }
+  function  (req, res, next) {
+    passport.authenticate('azuread-openidconnect',
+      {
+        response: res,
+        prompt: 'login',
+        failureRedirect: '/',
+        failureFlash: true,
+        successRedirect: '/'
+      }
+    )(req,res,next);
   }
 );
 
-router.get('/callback',
-  async function(req, res) {
-    const tokenRequest = {
-      code: req.query.code,
-      scopes: process.env.OAUTH_SCOPES.split(','),
-      redirectUri: process.env.OAUTH_REDIRECT_URI
-    };
-
-    try {
-      const response = await req.app.locals
-        .msalClient.acquireTokenByCode(tokenRequest);
-
-      // TEMPORARY!
-      // Flash the access token for testing purposes
-      req.flash('error_msg', {
-        message: 'Access token',
-        debug: response.accessToken
-      });
-    } catch (error) {
-      req.flash('error_msg', {
-        message: 'Error completing authentication',
-        debug: JSON.stringify(error, Object.getOwnPropertyNames(error))
-      });
-    }
-
-    res.redirect('/');
+// <CallbackRouteSnippet>
+router.post('/callback',
+  function(req, res, next) {
+    passport.authenticate('azuread-openidconnect',
+      {
+        response: res,
+        failureRedirect: '/',
+        failureFlash: true,
+        successRedirect: '/'
+      }
+    )(req,res,next);
   }
 );
+// </CallbackRouteSnippet>
 
 router.get('/signout',
-  async function(req, res) {
-    // Sign out
-    if (req.session.userId) {
-      // Look up the user's account in the cache
-      const accounts = await req.app.locals.msalClient
-        .getTokenCache()
-        .getAllAccounts();
-
-      const userAccount = accounts.find(a => a.homeAccountId === req.session.userId);
-
-      // Remove the account
-      if (userAccount) {
-        req.app.locals.msalClient
-          .getTokenCache()
-          .removeAccount(userAccount);
-      }
-    }
-
-    // Destroy the user's session
-    req.session.destroy(function (err) {
+  function(req, res) {
+    req.session.destroy(function(err) {
+      req.logout();
       res.redirect('/');
     });
   }
